a8eba6a623fbb8a2d72b8af478cdb57b70af2d52
Deep Prajapati <dprajap2@gmail.com>
2021-04-21 16:32:30 -0400

    Creates basic website and adds links

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <h1> Hi I'm Deep Prajapati a Science Minded Tech Enthusiast</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h3> About Me</h3>
+          I graduated from college with a degree in Neuroscience and worked as a research assistant at a translational research lab and as a Psychometrician. At both places I was exposed to how software works hand in hand with science to achieve results. I wanted to play a bigger role in the building process of those programs to help solve problems across all disciplines. During quarantine I took the time to learn the basics of coding and decided to make the jump from pre-health to software development. I joined GraceHopper Program to develop the skill set I need to learn quickly and efficiently. I bring my skills of inquiry, collaboration and attention to detail to web-development.
+
         </p>
+        <span>
+          Connect With Me
+        </span>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="github-link"
+          href="https://https://github.com/dprajapati4"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          github
+        </a>
+        <a
+          className="linkedIn-link"
+          href="https://www.linkedin.com/in/deepprajapati/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          linkedIn
         </a>
       </header>
     </div>
